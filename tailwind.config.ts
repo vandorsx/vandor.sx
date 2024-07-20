8fcbf61f5abdba99fab40bca06b9ff5c311d46f1
Jade van Dorsten <jade@inthetrees.me>
2024-07-19 22:41:32 -0500

    Add custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['seven sans', ...defaultTheme.fontFamily.sans],
+				serif: ['seven serif', ...defaultTheme.fontFamily.serif],
+			}
+		},
 	},
 	plugins: [],
 } satisfies Config
