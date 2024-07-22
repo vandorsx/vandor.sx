5f44b41d364c48f25d063855d3e33791e9c09853
Jade van Dorsten <jade@inthetrees.me>
2024-07-21 20:48:59 -0500

    Enable prefetching
    (prefetching all links by default)

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "hybrid",
-  adapter: cloudflare()
+   integrations: [tailwind()],
+   output: "hybrid",
+   adapter: cloudflare(),
+   prefetch: {
+      prefetchAll: true,
+      defaultStrategy: "hover",
+   },
 });
