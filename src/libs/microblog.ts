d7671a7a987ec7213b9a931f85addb914ae96d7e
Jade van Dorsten <jade@inthetrees.me>
2024-07-22 17:02:35 -0500

    Add getPost function

--- src/libs/microblog.ts
+++ src/libs/microblog.ts
@@ -44,3 +44,57 @@ export async function getPosts(feed: string) {
       return json.items;
    }
 }
+
+export const getPost = async (id: string, dateParams?: { year: string; month: string; day: string }) => {
+   const idDataResponse = await fetch(
+      `https://${import.meta.env.MICROBLOG_BASE_URL}/api/ids.json`,
+   );
+
+   if (!idDataResponse.ok) {
+      throw new Error("Failed to fetch id data");
+   } else {
+      const idData: {
+         ids: { id: string; url: string; date_published: string; date_modified: string }[];
+      } = await idDataResponse.json();
+
+      let postUrl: string | undefined = undefined;
+      if (
+         dateParams &&
+         dateParams.year &&
+         dateParams.month &&
+         dateParams.day &&
+         id
+      ) {
+         for (const item of idData.ids) {
+            const postDate = new Date(item.date_published.split("T")[0]);
+            if (
+               item.id === id &&
+               postDate.getUTCFullYear() === parseInt(dateParams.year) &&
+               postDate.getUTCMonth() + 1 === parseInt(dateParams.month) &&
+               postDate.getUTCDate() === parseInt(dateParams.day)
+            ) {
+               postUrl = item.url;
+               break;
+            }
+         }
+      } else {
+         postUrl = idData.ids.find((item) => item.id === id)?.url;
+      }
+
+      if (!postUrl) {
+         throw new Error(`Failed to find post with id: ${id}`);
+      } else {
+         let urlParts = new URL(postUrl);
+         urlParts.pathname = "/api" + urlParts.pathname;
+         const apiUrl = urlParts.toString();
+
+         const res = await fetch(apiUrl);
+         if (!res.ok) {
+            throw new Error(`Failed to fetch post from ${apiUrl}`);
+         } else {
+            const json = await res.json();
+            return json;
+         }
+      }
+   }
+};
