ff6421f3bb05b52927d47476d332f3d48b8a907e
Jade van Dorsten <jade@inthetrees.me>
2024-07-22 13:22:26 -0500

    Install SolidJS integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [tailwind()],
-   output: "hybrid",
-   adapter: cloudflare(),
-   prefetch: {
-      prefetchAll: true,
-      defaultStrategy: "hover",
-   },
+  integrations: [tailwind(), solidJs()],
+  output: "hybrid",
+  adapter: cloudflare(),
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover"
+  }
 });
