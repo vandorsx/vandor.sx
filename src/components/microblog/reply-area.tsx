810f327b9bee0969650174d6b599121e5e18ab33
Jade van Dorsten <jade@inthetrees.me>
2024-07-24 16:22:38 -0500

    Fix replies hydration error

--- src/components/microblog/reply-area.tsx
+++ src/components/microblog/reply-area.tsx
@@ -1,42 +1,50 @@
-import { createSignal, createEffect, onMount, For } from "solid-js";
-import { getMicrodotblog, type Microdotblog } from "~libs/microblog";
+import { createSignal, createEffect, onMount, For, onCleanup } from "solid-js";
+import {
+   getMicrodotblog,
+   type Microdotblog,
+   type MicrodotblogReply,
+} from "~libs/microblog";
 import sanitizeHtml from "sanitize-html";
-import DynamicTimestamp from "~components/microblog/dynamic-timestamp";
+import { formatTimeRelatively } from "~libs/relative-time";
 
 const [token, setToken] = createSignal("");
 const [username, setUsername] = createSignal("");
 const [tokenized, setTokenized] = createSignal(false);
 
-function Converastion(props: { microdotblog: Microdotblog }) {
-   const microdotblog = props.microdotblog;
+function Reply(props: { reply: MicrodotblogReply }) {
+   const reply = props.reply;
+
+   const [relativeTimestamp, setRelativeTimestamp] = createSignal(
+      formatTimeRelatively(reply.date_published),
+   );
+
+   const interval = setInterval(
+      () => setRelativeTimestamp(formatTimeRelatively(reply.date_published)),
+      1000,
+   );
+   onCleanup(() => clearInterval(interval));
 
    return (
-      <div class="flex flex-col gap-4 text-slightly-smaller">
-         <For each={microdotblog.items}>
-            {(reply) => (
-               <div class="flex flex-col">
-                  <div class="reply-metadata font-serif italic">
-                     {reply.author.url ?
-                        <a href={reply.author.url} target="_blank">
-                           {reply.author.name}
-                        </a>
-                     :  <span>{reply.author.name}</span>}
-                     {reply.author._microblog.username ===
-                        import.meta.env.MICROBLOG_USERNAME && (
-                        <span class="text-black/[.54]"> (me)</span>
-                     )}
-                     <span class="text-black/[.72]">
-                        {" "}
-                        replied <DynamicTimestamp date={reply.date_published} />
-                     </span>
-                  </div>
-                  <div
-                     class="reply-content"
-                     innerHTML={sanitizeHtml(reply.content_html)}
-                  />
-               </div>
+      <div class="flex flex-col">
+         <div class="reply-metadata font-serif italic">
+            {reply.author.url ?
+               <a href={reply.author.url} target="_blank">
+                  {reply.author.name}
+               </a>
+            :  <span>{reply.author.name}</span>}
+            {reply.author._microblog.username ===
+               import.meta.env.MICROBLOG_USERNAME && (
+               <span class="text-black/[.54]"> (me)</span>
             )}
-         </For>
+            <span class="text-black/[.72]">
+               {" "}
+               replied <span>{relativeTimestamp()}</span>
+            </span>
+         </div>
+         <div
+            class="reply-content"
+            innerHTML={sanitizeHtml(reply.content_html)}
+         />
       </div>
    );
 }
@@ -148,7 +156,11 @@ export function ReplyArea(props: {
          :  null}
          {microdotblog() && microdotblog().items.length > 0 ?
             <div class="reply-area-part">
-               <Converastion microdotblog={microdotblog()} />
+               <div class="flex flex-col gap-4 text-slightly-smaller">
+                  <For each={microdotblog().items}>
+                     {(reply) => <Reply reply={reply} />}
+                  </For>
+               </div>
             </div>
          :  null}
       </>
