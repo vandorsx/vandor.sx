3b46d6d444aa21aed95060986ac56a2245921ad5
Jade van Dörsten <vcs@vandor.sx>
2024-11-20 20:31:29 -0600

    Add blog sitemap to index

--- src/pages/sitemap_index.xml.ts
+++ src/pages/sitemap_index.xml.ts
@@ -1,10 +1,14 @@
+export const prerender = false;
 import type { APIRoute } from "astro";
+import { GET as BlogGET } from "./blog/lastmod.json.ts";
+
+const SITE = "https://vandor.sx";
 
 type lastmod = {
    lastmod: string;
 };
 
-export const GET: APIRoute = async ({ request }) => {
+export const GET: APIRoute = async (context) => {
    let microblog_lastmod: lastmod = { lastmod: "" };
    try {
       const res = await fetch(
@@ -21,11 +25,29 @@ export const GET: APIRoute = async ({ request }) => {
       console.error(e);
    }
 
+   let blog_lastmod: lastmod = { lastmod: "" };
+   try {
+      const res = await BlogGET(context);
+
+      if (!res.ok) {
+         throw new Error(`Failed to fetch blog lastmod: ${res.status}`);
+      } else {
+         const json = await res.json();
+         blog_lastmod = { lastmod: json.lastmod };
+      }
+   } catch (e) {
+      console.error(e);
+   }
+
    const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
     <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <sitemap>
         <loc>https://microblog.vandor.sx/sitemap.xml</loc>
         ${microblog_lastmod.lastmod ? `<lastmod>${microblog_lastmod.lastmod}</lastmod>` : ""}
+      </sitemap>
+      <sitemap>
+         <loc>${SITE}/blog/sitemap.xml</loc>
+         ${blog_lastmod.lastmod ? `<lastmod>${blog_lastmod.lastmod}</lastmod>` : ""}
       </sitemap>
     </sitemapindex>`;
 
