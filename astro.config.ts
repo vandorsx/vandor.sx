0b079fc3482222a604c7e9496d79bb77dfd40cf8
Jade van Dörsten <vcs@vandor.sx>
2025-04-12 11:37:05 -0500

    Install and use Cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+
     output: "static",
+
     build: {
         format: "file",
     },
+
     prefetch: {
         prefetchAll: true,
         defaultStrategy: "hover",
     },
+
     site: "https://vandor.sx",
+    adapter: cloudflare(),
 });
