cbe37a016e366914415ba92fee224b9b2513e1b7
Jade van Dörsten <vcs@vandor.sx>
2025-05-11 17:11:44 -0500

    Create navigation component

---
+++ src/components/navigation.tsx
@@ -0,0 +1,34 @@
+type NavigationProps = {
+    Links: {
+        href: string;
+        text: string;
+    }[];
+};
+
+export default function Navigation({ Links }: NavigationProps) {
+    return (
+        <div class="mb-7 flex gap-6 border-b px-1.5 pb-1.5 align-middle sm:gap-8 md:gap-16 lg:gap-24">
+            <span class="text-[13px] font-semibold sm:text-[15px]">
+                vandorsx
+            </span>
+            <nav class="text-[13px] sm:text-[15px]">
+                <ol class="flex items-center">
+                    {Links.map((link, index) => (
+                        <li class="flex items-center">
+                            {index === 0 && <span>~&nbsp;/&nbsp;</span>}
+                            {index === Links.length - 1 ?
+                                <span>{link.text}</span>
+                            :   <a href={link.href} class="hyperlink">
+                                    {link.text}
+                                </a>
+                            }
+                            {index !== Links.length - 1 && (
+                                <span>&nbsp;/&nbsp;</span>
+                            )}
+                        </li>
+                    ))}
+                </ol>
+            </nav>
+        </div>
+    );
+}
