5d0d34438e2277da441b389bffbb21b83a8189df
Jade van Dörsten <vcs@vandor.sx>
2025-05-11 14:35:09 -0500

    Add SolidJS integration

--- astro.config.ts
+++ astro.config.ts
@@ -2,23 +2,26 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
 
-    output: "static",
+  output: "static",
 
-    build: {
-        format: "file",
-    },
+  build: {
+      format: "file",
+  },
 
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: "hover",
-    },
+  prefetch: {
+      prefetchAll: true,
+      defaultStrategy: "hover",
+  },
 
-    site: "https://vandor.sx",
-    adapter: cloudflare(),
+  site: "https://vandor.sx",
+  adapter: cloudflare(),
+  integrations: [solidJs()],
 });
