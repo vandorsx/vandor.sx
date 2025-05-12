ad57ea84cf61c0fbb8b0fdfe575df5af2224aa56
Jade van Dörsten <vcs@vandor.sx>
2025-05-12 13:42:21 -0500

    Re-add `dynamic-timestamp.tsx` component

---
+++ src/components/microblog/dynamic-timestamp.tsx
@@ -0,0 +1,19 @@
+import { createSignal, onCleanup } from "solid-js";
+import { formatTimeRelatively } from "~libs/relative-time";
+
+interface Props {
+    date: string;
+}
+
+export default function DynamicTimestamp(props: Props) {
+    const [relativeTimestamp, setRelativeTimestamp] = createSignal(
+        formatTimeRelatively(props.date),
+    );
+    const interval = setInterval(
+        () => setRelativeTimestamp(formatTimeRelatively(props.date)),
+        1000,
+    );
+    onCleanup(() => clearInterval(interval));
+
+    return <span>{relativeTimestamp()}</span>;
+}
