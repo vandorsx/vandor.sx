dc0d9200922eacd0892b40c666480a1685c4c215
Jade van Dörsten <vcs@vandor.sx>
2025-05-12 00:02:18 -0500

    Update image carousel component

--- src/components/image-carousel.tsx
+++ src/components/image-carousel.tsx
@@ -9,27 +9,39 @@ type CarouselProps = {
 
 export default function ImageCarousel({ Photos }: CarouselProps) {
     return (
-        <div class="overflow-x-scroll overflow-y-hidden whitespace-nowrap">
-            {Photos.map((photo) => {
-                const fallbackExt = photo.exts[photo.exts.length - 1];
+        <div>
+            <span class="text-figure-outline text-right text-[12px]">
+                {Photos.length} images
+            </span>
+            <div class="border-figure-outline border p-1.5">
+                <div class="overflow-x-scroll overflow-y-hidden whitespace-nowrap">
+                    {Photos.map((photo) => {
+                        const fallbackExt = photo.exts[photo.exts.length - 1];
 
-                return (
-                    <picture class="inline-block">
-                        {photo.exts.map((ext) => (
-                            <source
-                                srcset={`${photo.urlWithoutExt}.${ext}`}
-                                type={`image/${ext}`}
-                            />
-                        ))}
-                        <img
-                            src={`${photo.urlWithoutExt}.${fallbackExt}`}
-                            alt={photo.alt}
-                            width={photo.dimensions[0]}
-                            height={photo.dimensions[1]}
-                        />
-                    </picture>
-                );
-            })}
+                        return (
+                            <div class="inline-block align-middle not-last:pr-1.5">
+                                <picture>
+                                    {photo.exts.map((ext) => (
+                                        <source
+                                            srcset={`${photo.urlWithoutExt}.${ext}`}
+                                            type={`image/${ext}`}
+                                            width={photo.dimensions[0]}
+                                            height={photo.dimensions[1]}
+                                        />
+                                    ))}
+                                    <img
+                                        src={`${photo.urlWithoutExt}.${fallbackExt}`}
+                                        alt={photo.alt}
+                                        width={photo.dimensions[0]}
+                                        height={photo.dimensions[1]}
+                                        class="h-auto max-h-[605px] w-auto"
+                                    />
+                                </picture>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
