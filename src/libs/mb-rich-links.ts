f4a8e8e40d0e742e86e387637f170b8ebab8ee31
Jade van Dörsten <vcs@vandor.sx>
2025-05-12 15:43:43 -0500

    Update rich link generation function

--- src/libs/mb-rich-links.ts
+++ src/libs/mb-rich-links.ts
@@ -1,57 +1,63 @@
 import * as cheerio from "cheerio";
 
 export const createRichLinks = (content_html: string): string => {
-   const $ = cheerio.load(`<div class="content-wrapper">${content_html}</div>`);
-   const richLinks = $(".rich-link");
+    const $ = cheerio.load(
+        `<div class="content-wrapper">${content_html}</div>`,
+    );
+    const richLinks = $(".rich-link");
 
-   richLinks.each(function () {
-      const richLink = $(this);
+    richLinks.each(function () {
+        const richLink = $(this);
 
-      const linkElement = richLink.find("cite a");
-      const url =
-         $(this).attr("cite") || linkElement.attr("href") || linkElement.text();
+        const linkElement = richLink.find("cite a");
+        const url =
+            $(this).attr("cite") ||
+            linkElement.attr("href") ||
+            linkElement.text();
 
-      const niceUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
-      linkElement.text(niceUrl);
-      linkElement.attr("target", "_blank");
-      linkElement.addClass("rich-link-a");
-   });
+        const niceUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
+        linkElement.text(niceUrl);
+        linkElement.attr("target", "_blank");
+        linkElement.addClass("rich-link-a");
+    });
 
-   return $(".content-wrapper").html() || content_html;
+    return $(".content-wrapper").html() || content_html;
 };
 
 export const createLegacyRichLinks = (content_html: string): string => {
-   const $ = cheerio.load(`<div class="content-wrapper">${content_html}</div>`);
-   const richLinkContainers = $(".rl-container");
+    const $ = cheerio.load(
+        `<div class="content-wrapper">${content_html}</div>`,
+    );
+    const richLinkContainers = $(".rl-container");
 
-   richLinkContainers.each(function () {
-      const richLink = $(this).find("blockquote");
+    richLinkContainers.each(function () {
+        const richLink = $(this).find("blockquote");
 
-      const url = richLink.attr("cite") || "";
-      const title = richLink.find(".rl-title").text() || "";
-      const snippet = richLink.find(".rl-snippet").text() || "";
+        const url = richLink.attr("cite") || "";
+        const title = richLink.find(".rl-title").text() || "";
+        const snippet = richLink.find(".rl-snippet").text() || "";
 
-      const niceUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
+        const niceUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
 
-      let innerHTML = "";
-      if (snippet.length > 0) {
-         innerHTML += `
-                <p class="rl-snippet">${snippet}</p>
+        let innerHTML = "";
+        if (snippet.length > 0) {
+            innerHTML += `
+                <p>${snippet}</p>
             `;
-      }
-      innerHTML += `
+        }
+        innerHTML += `
                      <footer>
                         ${title ? `<span>${title}</span>` : ""}
                         <div>
-                           <cite><a href="${url}" target="_blank" class="rich-link-a">${niceUrl}</a></cite>
+                           <cite><a href="${url}" target="_blank">${niceUrl}</a></cite>
                         </div>
                      </footer>
                   `;
 
-      richLink.html(innerHTML);
-      richLink.addClass("rich-link");
-      richLink.unwrap();
-   });
+        richLink.html(innerHTML);
+        richLink.addClass("mb-rlink");
+        richLink.unwrap();
+    });
 
-   return $(".content-wrapper").html() || content_html;
+    return $(".content-wrapper").html() || content_html;
 };
