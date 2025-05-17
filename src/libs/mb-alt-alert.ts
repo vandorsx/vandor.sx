d71ac1505cd6337d036f31087a83f53acf52acb4
Jade van Dörsten <vcs@vandor.sx>
2025-05-17 17:34:33 -0500

    Clean up merge

--- src/libs/mb-alt-alert.ts
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", (e) => {
-   const target = e.target as HTMLElement;
-   if (target.matches("button[data-alt]")) {
-      const altText = target.getAttribute("data-alt");
-      if (altText) {
-         alert(altText);
-      }
-   }
-});
