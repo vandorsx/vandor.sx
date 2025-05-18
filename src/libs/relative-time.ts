bdf4ea62f06263f5f08cb256d6ac4d049c9e6215
Jade van Dörsten <vcs@vandor.sx>
2025-05-17 22:41:14 -0500

    Clean up lint for `relative-time.ts`

--- src/libs/relative-time.ts
+++ src/libs/relative-time.ts
@@ -1,34 +1,31 @@
 const rel_format: Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat("en", {
-   numeric: "always",
+    numeric: "always",
 });
 const rel_units: { unit: Intl.RelativeTimeFormatUnit; seconds: number }[] = [
-   { unit: "year", seconds: 31540000 },
-   { unit: "month", seconds: 2628000 },
-   { unit: "week", seconds: 604800 },
-   { unit: "day", seconds: 86400 },
-   { unit: "hour", seconds: 3600 },
-   { unit: "minute", seconds: 60 },
-   { unit: "second", seconds: 1 },
+    { unit: "year", seconds: 31540000 },
+    { unit: "month", seconds: 2628000 },
+    { unit: "week", seconds: 604800 },
+    { unit: "day", seconds: 86400 },
+    { unit: "hour", seconds: 3600 },
+    { unit: "minute", seconds: 60 },
+    { unit: "second", seconds: 1 },
 ];
 
-export const formatTimeRelatively = (date: Date | string): string => {
-   const now: Date = new Date();
+export const formatTimeRelatively = (input: Date | string): string => {
+    const now = new Date();
 
-   if (typeof date === "string") {
-      date = new Date(date);
-   }
+    const date = typeof input === "string" ? new Date(input) : input;
 
-   const diff = (date.getTime() - now.getTime()) / 1000;
+    const diffSeconds = (date.getTime() - now.getTime()) / 1000;
+    if (Math.abs(diffSeconds) < 60) {
+        return "less than a minute ago";
+    }
 
-   if (Math.abs(diff) < 60) {
-      return "less than a minute ago";
-   }
+    for (const { unit, seconds } of rel_units) {
+        if (Math.abs(diffSeconds) >= seconds) {
+            return rel_format.format(Math.round(diffSeconds / seconds), unit);
+        }
+    }
 
-   for (const { unit, seconds } of rel_units) {
-      if (Math.abs(diff) >= seconds) {
-         return rel_format.format(Math.round(diff / seconds), unit);
-      }
-   }
-
-   return "date error";
+    return "date error";
 };
